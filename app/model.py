22116147ea551706d3353aeeda29bb151d91ac8d
mavedev <usatchoff@gmail.com>
2020-03-24 23:21:11 +0200

    Post model added

--- app/model.py
+++ app/model.py
@@ -1,4 +1,19 @@
+from datetime import datetime
+
 from flask_sqlalchemy import SQLAlchemy
 
-
 db = SQLAlchemy()
+
+
+class Post(db.Model):  # type: ignore
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140))
+    body = db.Column(db.Text)
+    created = db.Column(db.DateTime, default=datetime.now())
+
+    def __init__(self, *args, **kwargs) -> None:
+        super(Post, self).__init__(*args, **kwargs)
+
+    def __repr__(self) -> str:
+        return '<Post {}>'.format(self.title)
