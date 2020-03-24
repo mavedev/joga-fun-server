3d08a236c23d6735ef41c6f5b6cb1cd415ee37ba
mavedev <usatchoff@gmail.com>
2020-03-24 20:58:10 +0200

    Flask initialization bug fixed

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,6 @@ from config import config
 
 
 def get_app(config_name: str) -> Flask:
-    app = Flask(__file__)
+    app = Flask(__name__)
     app.config.from_object(config[config_name])
     return app
