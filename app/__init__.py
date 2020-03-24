d07fe6f3be46f9335d281bab87e6be79960c20ea
mavedev <usatchoff@gmail.com>
2020-03-24 20:57:13 +0200

    App initialization added

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from config import config
+
+
+def get_app(config_name: str) -> Flask:
+    app = Flask(__file__)
+    app.config.from_object(config[config_name])
+    return app
