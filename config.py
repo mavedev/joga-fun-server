6a5bfb5e7d173eb54665b2ee417e247d91d1478e
mavedev <usatchoff@gmail.com>
2020-03-25 21:17:59 +0200

    Config changed

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 
 
 class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY') or os.urandom(24).hex()
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
@@ -10,17 +11,27 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = (
         os.getenv('DEV_DB_URL')
-        or 'postgresql+psycopg2://postgres:{password}@localhost/Customers'
+        or 'postgresql+psycopg2://postgres:{password}@localhost/joga_dev'
         .format(password=os.getenv('DEV_DB_PASS') or '')
     )
 
 
 class TestingConfig(Config):
-    pass
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = (
+        os.getenv('DEV_DB_URL')
+        or 'postgresql+psycopg2://postgres:{password}@localhost/joga_test'
+        .format(password=os.getenv('DEV_DB_PASS') or '')
+    )
 
 
 class ProductionConfig(Config):
-    pass
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = (
+        os.getenv('DEV_DB_URL')
+        or 'postgresql+psycopg2://postgres:{password}@localhost/joga_prod'
+        .format(password=os.getenv('DEV_DB_PASS') or '')
+    )
 
 
 config: Dict[str, type] = {
