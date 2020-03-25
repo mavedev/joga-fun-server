958d2a1b549c7572badcdcb4d3aee112c097d567
mavedev <usatchoff@gmail.com>
2020-03-25 19:36:34 +0200

    Roles added

--- app/model.py
+++ app/model.py
@@ -1,13 +1,29 @@
 from datetime import datetime
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_security import RoleMixin
 
 db = SQLAlchemy()
+roles_users = db.Table(
+    'roles_users',
+    db.Column('user_id', db.Integer(), db.ForeignKey('users.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('roles.id'))
+)
+
+
+class Role(db.Model, RoleMixin):  # type: ignore
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+    def __repr__(self):
+        return '<Role {}>'.format(self.name)
 
 
 class Post(db.Model):  # type: ignore
     __tablename__ = 'posts'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(140))
     body = db.Column(db.Text)
     created = db.Column(db.DateTime, default=datetime.now())
