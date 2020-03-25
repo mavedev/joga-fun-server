54d885a79ab0084f62beef59bf5c8caa6f3bd5ac
mavedev <usatchoff@gmail.com>
2020-03-25 20:52:17 +0200

    Get posts method added

--- app/api/posts.py
+++ app/api/posts.py
@@ -1,11 +1,12 @@
 from flask import jsonify
 
 from . import api
+from ..model import db  # noqa
 
 
-@api.route('/posts/<int:number>/')
+@api.route('/posts/<int:how_many>')
 def get_posts(how_many: int) -> str:
     return jsonify({
-        'name': 'Ciri',
+        'name': 'Test',
         'status': 'happy'
     })
