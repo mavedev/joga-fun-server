ff1809505bbb9a75b759bfab5db2d9169b42d007
mavedev <usatchoff@gmail.com>
2020-03-25 21:36:43 +0200

    Post fields auto fill fix

--- app/model.py
+++ app/model.py
@@ -65,7 +65,7 @@ class Post(db.Model):  # type: ignore
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(_TEXT_LEN_MID))
     body = db.Column(db.Text)
-    created = db.Column(db.DateTime, default=datetime.now())
+    created = db.Column(db.DateTime, default=datetime.now)
 
     def __repr__(self) -> str:
         return '<Post {}>'.format(self.title)
