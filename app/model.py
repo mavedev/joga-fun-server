8e90bc9ce8180c971bd5bd5db30355b51bfe5b74
mavedev <usatchoff@gmail.com>
2020-03-25 19:37:33 +0200

    User added

--- app/model.py
+++ app/model.py
@@ -21,6 +21,24 @@ class Role(db.Model, RoleMixin):  # type: ignore
         return '<Role {}>'.format(self.name)
 
 
+class User(db.Model):  # type: ignore
+    __tablename__ = 'users'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100))
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password_hash = db.Column(db.String(100), nullable=False)
+    created_on = db.Column(db.DateTime(), default=datetime.utcnow)
+    updated_on = db.Column(
+        db.DateTime(),
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
+
+    def __repr__(self):
+        return "<{}:{}>".format(self.id, self.username)
+
+
 class Post(db.Model):  # type: ignore
     __tablename__ = 'posts'
     id = db.Column(db.Integer(), primary_key=True)
