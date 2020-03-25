6ed3076bb23b6a817073c3847bc6dffe2ae35059
mavedev <usatchoff@gmail.com>
2020-03-25 21:02:50 +0200

    App managing restructured

--- manage.py
+++ manage.py
@@ -1,13 +1,25 @@
 import os
 
-from app import create_app
+from flask_script import Manager, Shell
 
+from app import create_app
+from app.model import db, User, Post
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+manager.add_command(
+    'shell',
+    Shell(make_context=lambda: {
+        'app': app,
+        'db': db,
+        'User': User,
+        'Post': Post
+    })
+)
 
 
 def main() -> None:
-    app.run()
+    manager.run()
 
 
 if __name__ == '__main__':
