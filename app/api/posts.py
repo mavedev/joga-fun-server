86dddfb2ae08414d44231799720c7a5e924bd15d
mavedev <usatchoff@gmail.com>
2020-03-25 22:05:14 +0200

    Posts JSON getting implemented

--- app/api/posts.py
+++ app/api/posts.py
@@ -1,12 +1,12 @@
 from flask import jsonify
 
 from . import api
-from ..model import db  # noqa
+from ..model import db, Post
 
 
 @api.route('/posts/<int:how_many>')
 def get_posts(how_many: int) -> str:
-    return jsonify({
-        'name': 'Test',
-        'status': 'happy'
-    })
+    return jsonify(results=[
+        post.to_json() for post in
+        db.session.query(Post).limit(how_many).all()
+    ])
