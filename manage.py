59412bf704d0a89ef2753f344e6cae78ce12d7d0
mavedev <usatchoff@gmail.com>
2020-03-26 12:45:48 +0200

    DB migration implemented

--- manage.py
+++ manage.py
@@ -1,13 +1,14 @@
 import os
 
 from flask_script import Manager, Shell
-from flask_migrate import MigrateCommand
+from flask_migrate import Migrate, MigrateCommand
 
 from app import create_app
 from app.model import db, User, Post
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
+migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 manager.add_command(
     'shell',
