6d5ee5ee8485ea6e9daff21a025170d23c289ed8
mavedev <usatchoff@gmail.com>
2020-04-05 18:18:31 +0300

    Post creation implemented

--- app/api/logic/posts.py
+++ app/api/logic/posts.py
@@ -1,7 +1,18 @@
 from typing import List
 
+from sqlalchemy.exc import SQLAlchemyError
+
 from app.model import db, Post
 
 
-def get_posts(how_many: int) -> List[Post]:
+def create_post(title: str, body: str) -> bool:
+    try:
+        db.session.add(Post(title=title, body=body))
+        db.session.commit()
+        return True
+    except SQLAlchemyError:
+        return False
+
+
+def read_posts(how_many: int) -> List[Post]:
     return db.session.query(Post).limit(how_many).all()
--- app/api/routes.py
+++ app/api/routes.py
@@ -1,17 +1,21 @@
-from flask import jsonify, request
+from flask import jsonify, request, Response
 
 from . import api
-from .post import get_posts
+from . import posts
 
 
 @api.route('/posts/<int:how_many>')
-def get_posts(how_many: int) -> str:
+def read_posts(how_many: int) -> str:
     return jsonify(results=[
         post.to_json() for post in
-        get_posts(how_many)
+        posts.read_posts(how_many)
     ])
 
 
 @api.route('posts/create', methods=['POST'])
 def create_post() -> str:
-    pass
+    result: bool = posts.create_post(
+        title=request['title'],
+        body=request['body']
+    )
+    return Response(status=200 if result else 500)
