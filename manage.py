22f7c462953fddf95f2ec7eb2b304f3f54726c97
Cirill Usatchoff <usatchoff@gmail.com>
2020-07-02 19:59:56 +0300

    Managing stuff moved to a separate function

--- manage.py
+++ manage.py
@@ -8,30 +8,35 @@ from flask_security import Security, SQLAlchemyUserDatastore
 from app import create_app
 from app.model import db, User, Post, Role
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-manager = Manager(app)
-migrate = Migrate(app, db)
-login_manager = LoginManager(app)
-datastore = SQLAlchemyUserDatastore(db, User, Role)
-security = Security(app, datastore)
-manager.add_command('db', MigrateCommand)
-manager.add_command(
-    'shell',
-    Shell(make_context=lambda: {
-        'app': app,
-        'db': db,
-        'User': User,
-        'Post': Post
-    })
-)
-
-
-@login_manager.user_loader
-def load_user(user_id: int) -> 'User':
-    return db.session.query(User).get(user_id)
-
 
 def main() -> None:
+    app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+    manager = Manager(app)
+    migrate = Migrate(app, db)
+    login_manager = LoginManager(app)
+    datastore = SQLAlchemyUserDatastore(db, User, Role)
+    security = Security(app, datastore)
+    manager.add_command('db', MigrateCommand)
+    manager.add_command(
+        'shell',
+        Shell(make_context=lambda: {
+            'db': db,
+            'app': app,
+            'User': User,
+            'Post': Post,
+            'manager': manager,
+            'migrate': migrate,
+            'security': security,
+            'datastore': datastore,
+            'login_manager': login_manager
+        })
+    )
+
+    @login_manager.user_loader
+    def load_user(user_id: int) -> User:
+        return db.session.query(User).get(user_id)
+
     manager.run()
 
 
