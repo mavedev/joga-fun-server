6954550e3c88c4701cb68bcc7f6872feac347f89
Cirill Usatchoff <usatchoff@gmail.com>
2020-07-02 19:46:06 +0300

    flask_security added

--- manage.py
+++ manage.py
@@ -2,13 +2,16 @@ import os
 
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
+from flask_security import Security, SQLAlchemyUserDatastore
 
 from app import create_app
-from app.model import db, User, Post
+from app.model import db, User, Post, Role
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
+datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, datastore)
 manager.add_command('db', MigrateCommand)
 manager.add_command(
     'shell',
