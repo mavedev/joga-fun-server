30949a4fc102de647dac5b7a5d7ec5f7584b8dda
Cirill Usatchoff <usatchoff@gmail.com>
2020-07-02 22:47:18 +0300

    Typo

--- app/constants.py
+++ app/constants.py
@@ -6,6 +6,6 @@ _TEXT_LEN_MAX: int = 255
 _TEXT_LEN_MID: int = 150
 _TEXT_LEN_MIN: int = 80
 
-# ALiases.
+# Aliases.
 JSONLike = Dict[str, Any]
 AuthHeader = Union[Dict[AnyStr, AnyStr], Authorization]
