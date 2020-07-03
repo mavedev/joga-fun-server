3ce8dfef4380cca648a3e86c3b602b9e3dd7612f
Cirill Usatchoff <usatchoff@gmail.com>
2020-07-03 19:52:55 +0300

    Auth JSON token generating provided

--- app/api/logic/auth.py
+++ app/api/logic/auth.py
@@ -1,6 +1,24 @@
+from datetime import datetime, timedelta
+from flask import current_app, jsonify
 from flask_security import login_user
+from jwt import encode
 
 from app.model import User
+from app.constants import (
+    JWT_EXP_DELTA_SECONDS,
+    JWT_ALGORITHM,
+    JSONLike
+)
+
+
+def get_token(user_id: int) -> JSONLike:
+    payload = {
+        'user_id': user_id,
+        'exp': datetime.utcnow() + timedelta(seconds=JWT_EXP_DELTA_SECONDS)
+    }
+    jwt_secret = current_app.config['SECRET_KEY']
+    jwt_token = encode(payload, jwt_secret, JWT_ALGORITHM)
+    return jsonify({'token': jwt_token.decode('utf-8')})
 
 
 def can_administrate(username: str, password: str) -> bool:
--- app/constants.py
+++ app/constants.py
@@ -2,9 +2,11 @@ from typing import Union, Dict, Any, AnyStr
 from werkzeug.datastructures import Authorization
 
 # Numbers.
-_TEXT_LEN_MAX: int = 255
-_TEXT_LEN_MID: int = 150
-_TEXT_LEN_MIN: int = 80
+_TEXT_LEN_MAX = 255
+_TEXT_LEN_MID = 150
+_TEXT_LEN_MIN = 80
+JWT_EXP_DELTA_SECONDS = 20
+JWT_ALGORITHM = 'HS256'
 
 # Aliases.
 JSONLike = Dict[str, Any]
